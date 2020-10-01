662a9e6676256f22bba9d7569496af0e7e7a5aa1
Thamara Chellakooty <thamaralinnea@gmail.com>
2020-10-01 12:48:11 +0200

    Game complete

--- src/Spil/DiceGame.java
+++ src/Spil/DiceGame.java
@@ -7,10 +7,10 @@ public class DiceGame {
     public static void main(String[] args) {
         //opretter 4 terninger så spillerne får to terninger hver
 
-        Dice Dice1 = new Dice();
-        Dice Dice2 = new Dice();
-        Dice Dice3 = new Dice();
-        Dice Dice4 = new Dice();
+        Dice Dice1 = new Dice(1);
+        Dice Dice2 = new Dice(1);
+        Dice Dice3 = new Dice(1);
+        Dice Dice4 = new Dice(1);
 
         //summen af terningerne der bliver slået
         int sum1;
@@ -23,14 +23,33 @@ public class DiceGame {
 
         //While loop der kører indtil spiller 1 ellers 2's sum er større eller lig med 40
 
-        while (p1sum <= 40 || p2sum <=40) {
+        while (p1sum <= 40 && p2sum <=40) {
             Scanner input = new Scanner(System.in);
             System.out.println("press r and enter to roll dice");
             String roll = input.nextLine();
             if (roll.equals("r")) {
                 Dice1.roll();
+                Dice2.roll();
+                Dice3.roll();
+                Dice4.roll();
 
+                //når en spiller slår med to terninger skal facevalue af terningerne slåes sammen
+
+                sum1 = Dice1.getFacevalue() + Dice2.getFacevalue();
+                sum2 = Dice3.getFacevalue() + Dice4.getFacevalue();
+
+                int dienr1 = Dice1.getFacevalue();
+                int dienr2 = Dice2.getFacevalue();
+                int dienr3 = Dice3.getFacevalue();
+                int dienr4 = Dice4.getFacevalue();
+                p1sum = p1sum + sum1;
+                p2sum = p2sum + sum2;
             }
+
+            System.out.println("Player 1: " + "Dice one: " + Dice1 + "  Dice two:  " + Dice2);
+            System.out.println("player 1 total = " + p1sum);
+            System.out.println("Player 2: " + "Dice one:" + Dice3 + " Dice two: "  + Dice4);
+            System.out.println("player 2 total=  " + p2sum);
 
         }
     }
