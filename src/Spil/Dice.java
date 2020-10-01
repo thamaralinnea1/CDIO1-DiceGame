e303a9681855e7eb139044c7b293ebe2f833a765
cboej <s205360@student.dtu.dk>
2020-10-01 11:28:43 +0200

    Dice

--- src/Spil/Dice.java
+++ src/Spil/Dice.java
@@ -5,6 +5,29 @@ public class Dice {
     private final int Max = 6; //terning får 6 sider
     private int facevalue;
 
-    public Dice(int value)
-    
+    public Dice(int value){
+        facevalue = value;
+
     }
+    //kast terningen og vis værdien
+    public int roll()
+    {
+        //+1 for at terningen viser 6
+        facevalue = (int)(Math.random()*Max)+ 1;
+
+        return facevalue;
+
+    }
+    //vis værdi
+
+
+    public void setFacevalue(int value){
+        facevalue = value;
+
+    }
+    //hent værdien
+    public int getFacevalue (){
+        return facevalue;
+
+    }
+}
