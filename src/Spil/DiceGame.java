9e792a97379a4ae1d70b0e3697e2bd3692773597
Pernille <pernille.lyngholm1@outlook.com>
2020-10-01 11:43:34 +0200

    Dice created in game

--- src/Spil/DiceGame.java
+++ src/Spil/DiceGame.java
@@ -1,5 +1,24 @@
 package Spil;
 
+import java.util.Scanner;
+
 public class DiceGame {
-    // first comment
+
+    public static void main(String[] args) {
+        //opretter 4 terninger så spillerne får to terninger hver
+
+        Dice Dice1 = new Dice();
+        Dice Dice2 = new Dice();
+        Dice Dice3 = new Dice();
+        Dice Dice4 = new Dice();
+
+        //summen af terningerne der bliver slået
+        int sum1;
+        int sum2;
+
+        //holder styr på spiller 1 og spiller 2's sum i løbet af spillet
+
+        int p1sum = 0;
+        int p2sum = 0;
+    }
 }
