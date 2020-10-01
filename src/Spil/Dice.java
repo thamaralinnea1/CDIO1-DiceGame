7f65790da7bd5945c5f8e8455582da7c12e5ff63
Thamara Chellakooty <thamaralinnea@gmail.com>
2020-10-01 11:10:18 +0200

    Test and Dice packages created

--- src/Spil/Dice.java
+++ src/Spil/Dice.java
@@ -1,10 +1,10 @@
-
+package Spil;
 
 public class Dice {
 
     private final int Max = 6; //terning får 6 sider
     private int facevalue;
 
-    public Dice(int.value)
+    public Dice(int value)
     
     }
